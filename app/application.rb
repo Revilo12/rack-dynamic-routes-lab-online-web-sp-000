2d222d7a7cb2e107f7b014aabd7b84a96e9eda4e
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-11 14:10:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path
+    if req.path.match('/items/'')
       search_item = /(?<=\/items\/).+/.match(req.path).to_s
       item = @@items.find {|item| item.name == search_item} if search_item
       if item
